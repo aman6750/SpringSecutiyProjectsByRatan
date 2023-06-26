885e11c2b8d264b235914db7c7a45ef80fb49452
Aman <amanyadav6750@gmail.com>
2023-06-27 02:04:45 +0530

    farzi

---
+++ springboot-1-udemy/src/main/java/com/udemy/springboot1udemy/bean/Post.java
@@ -0,0 +1,12 @@
+package com.udemy.springboot1udemy.bean;
+
+@data
+public class Post {
+	
+	private long post_id;
+	private String title;
+	private String description;
+	private String content;
+	
+
+}
